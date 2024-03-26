f007fa44f051850d3b1d5fb38df56ad413e3b4db
Davlat <davlatberdinazarov0708@gmail.com>
2024-03-26 16:31:25 +0500

    Modified sign up sign in password sections and maked dynamic

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Input } from "@material-tailwind/react";
 import { PatternFormat } from "react-number-format";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-
+import { ProjectContext } from "../layout/MainLayout";
+import { IoEyeOff } from "react-icons/io5";
+import { IoEye } from "react-icons/io5";
 
 export default function Register() {
   const [phone, setPhone] = useState("");
@@ -11,19 +13,25 @@ export default function Register() {
   const [pass, setPass] = useState("");
   const [r_pass, setR_pass] = useState("");
   const [errorMessage, setErrorMessage] = useState(false);
+  const [isEyeOff, setIsEyeOff] = useState(true);
+  const [isEyeOff_R, setIsEyeOff_R] = useState(true);
 
-  const data = { name, phone, pass, r_pass};  
-  
+  const { setEnteredPhone } = useContext(ProjectContext);
+
+  const data = { name, phone, pass, r_pass };
+
   const navigate = useNavigate();
 
   const onsubmit = (e) => {
     e.preventDefault();
-    if(!(/^[a-zA-Z]{3,}$/.test(name))){
-      return setErrorMessage("Name invalid")
+    if (!/^[a-zA-Z]{3,}$/.test(name)) {
+      return setErrorMessage("Name invalid");
     }
-    if(pass !== r_pass){
-      return setErrorMessage("password birxil emas")
+    if (pass !== r_pass) {
+      return setErrorMessage("password birxil emas");
     }
+
+    setEnteredPhone(phone);
 
     axios
       .post("https://itlive.introdevs.site/api/verification/send", {
@@ -62,7 +70,6 @@ export default function Register() {
                 Ro'yxatdan o'tish
               </h1>
               <form className=" h-[470px]">
-
                 {/* error alert */}
                 {errorMessage ? (
                   <div
@@ -118,49 +125,75 @@ export default function Register() {
                     }}
                   />
                 </div>
-                <div className="my-4">
+                <div class="w-full">
                   <label
                     className=" text-plum font-semibold"
                     htmlFor="password"
                   >
                     Parol
                   </label>
-                  <Input
-                    id="password"
-                    type="password"
-                    onChange={handlerPass}
-                    placeholder="Parol"
-                    className=" text-lg !border !border-gray-300 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
-                    labelProps={{
-                      className: "hidden",
-                    }}
-                    containerProps={{
-                      className: "min-w-[300px] my-2 h-[60px] md:w-[450px]",
-                    }}
-                  />
+                  <div class="relative w-full bg-white">
+                    <div
+                      onClick={() => setIsEyeOff((prev) => !prev)}
+                      class="absolute grid w-8 h-8 place-items-center text-plum top-2/4 right-6 -translate-y-2/4 z-40"
+                    >
+                      {isEyeOff ? (
+                        <IoEyeOff className="text-2xl" />
+                      ) : (
+                        <IoEye className="text-2xl" />
+                      )}
+                    </div>
+                    <Input
+                      className="!border !border-gray-300 peer w-full h-full bg-transparent font-sans font-normal outline outline-0 focus:outline-0 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] !pr-9 text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+                      id="password"
+                      type={isEyeOff ? "password" : "text"}
+                      onChange={handlerPass}
+                      placeholder="Parol"
+                      labelProps={{
+                        className: "hidden",
+                      }}
+                      containerProps={{
+                        className:
+                          " min-w-[300px] my-2 h-[60px] md:w-[450px] text-lg",
+                      }}
+                    />
+                  </div>
                 </div>
-                <div className="my-4">
+                <div class="w-full">
                   <label
                     className=" text-plum font-semibold"
                     htmlFor="password_R"
                   >
                     Parolni tasdiqlash
                   </label>
-                  <Input
-                    id="password_R"
-                    type="password"
-                    onChange={handlerR_pass}
-                    placeholder="Tasdiqlash"
-                    className=" text-lg !border !border-gray-300 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
-                    labelProps={{
-                      className: "hidden",
-                    }}
-                    containerProps={{
-                      className: "min-w-[300px] my-2 h-[60px] md:w-[450px]",
-                    }}
-                    required
-                  />
+                  <div class="relative w-full bg-white">
+                    <div
+                      onClick={() => setIsEyeOff_R((prev) => !prev)}
+                      class="absolute grid w-8 h-8 place-items-center text-plum top-2/4 right-6 -translate-y-2/4 z-40"
+                    >
+                      {isEyeOff_R ? (
+                        <IoEyeOff className="text-2xl" />
+                      ) : (
+                        <IoEye className="text-2xl" />
+                      )}
+                    </div>
+                    <Input
+                      className="!border !border-gray-300 peer w-full h-full bg-transparent font-sans font-normal outline outline-0 focus:outline-0 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] !pr-9 text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+                      type={isEyeOff_R ? "password" : "text"}
+                      id="password_R"
+                      onChange={handlerR_pass}
+                      placeholder="Tasdiqlash"
+                      labelProps={{
+                        className: "hidden",
+                      }}
+                      containerProps={{
+                        className:
+                          " min-w-[300px] my-2 h-[60px] md:w-[450px] text-lg",
+                      }}
+                    />
+                  </div>
                 </div>
+
                 <div className="flex items-center justify-between z-10">
                   <button
                     onClick={onsubmit}
